1135610b10bfdf955dbe75157bca36588829e3f8
HOLA123XD8 <ocana.jalejandro@gmail.com>
2026-05-19 20:54:01 -0600

    ejercicio 1.3 completo

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,54 @@ const Header = (props) => {
   )
 }
 
-// 1. NUEVO COMPONENTE: Se encarga de pintar UNA sola parte
 const Part = (props) => {
   return (
     <p>{props.name} {props.exercises}</p>
   )
 }
 
-// 2. COMPONENTE CONTENT: Ahora usa el componente Part tres veces
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
+      {/* 2. Como ahora recibimos objetos completos, accedemos a sus propiedades con el punto (.) */}
+      <Part name={props.part1.name} exercises={props.part1.exercises} />
+      <Part name={props.part2.name} exercises={props.part2.exercises} />
+      <Part name={props.part3.name} exercises={props.part3.exercises} />
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.total}</p>
+    /* 3. Lo mismo aquí: accedemos a los números sumando las propiedades del objeto */
+    <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  // 1. TRANSFORMACIÓN: Convertimos las variables sueltas en Objetos
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3} 
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      
+      {/* 4. Le pasamos los objetos enteros directamente a Content y Total */}
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   )
 }
