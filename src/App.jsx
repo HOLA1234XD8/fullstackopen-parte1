c34e1844b340034cd2c03fc64c0843cce725ec7c
HOLA123XD8 <ocana.jalejandro@gmail.com>
2026-05-19 21:00:38 -0600

    ejercicio 1.4 completo

--- src/App.jsx
+++ src/App.jsx
@@ -13,45 +13,46 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      {/* 2. Como ahora recibimos objetos completos, accedemos a sus propiedades con el punto (.) */}
-      <Part name={props.part1.name} exercises={props.part1.exercises} />
-      <Part name={props.part2.name} exercises={props.part2.exercises} />
-      <Part name={props.part3.name} exercises={props.part3.exercises} />
+      {/* 2. Accedemos a cada objeto usando su índice en el arreglo [0, 1, 2] */}
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    /* 3. Lo mismo aquí: accedemos a los números sumando las propiedades del objeto */
-    <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+    /* 3. Sumamos los ejercicios accediendo a cada posición del arreglo */
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
   
-  // 1. TRANSFORMACIÓN: Convertimos las variables sueltas en Objetos
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  // 1. TRANSFORMACIÓN: Ahora es un solo arreglo llamado 'parts' que contiene los 3 objetos
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      
-      {/* 4. Le pasamos los objetos enteros directamente a Content y Total */}
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      {/* 4. Pasamos el arreglo completo bajo la prop 'parts' */}
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
